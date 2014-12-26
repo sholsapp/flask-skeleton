c8bdac8988628b53021cf94b6706679052829a20
Stephen Holsapple <sholsapp@gmail.com>
2014-12-25 21:16:43 -0800

    Fix setup.py to have new flask-skeleton name

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open(os.path.abspath('README.md')) as fh:
   README = fh.read()
 
 setup(
-  name='flask-heroku',
+  name='flask-skeleton',
   version='1.0',
   description=README,
   author='Stephen Holsapple',
   author_email='sholsapp@gmail.com',
   url='http://www.flask.com',
-  packages=['flaskheroku'],
+  packages=['flaskskeleton'],
   install_requires=[
     'configobj',
     'validate',

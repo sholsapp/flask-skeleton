3e961032fb0bf984256412aeb0dd519fe18bee55
Stephen Holsapple <sholsapp@gmail.com>
2015-06-30 21:23:44 -0700

    Add a manage.py command to start a worker.
    This starts a simple background worker that executes every second.

--- manage.py
+++ manage.py
@@ -3,12 +3,32 @@
 from configobj import ConfigObj
 from validate import Validator
 from flask.ext.script import Manager
+import logging
 
 from flaskskeleton import app, init_webapp
 from flaskskeleton.model import db, Messages
+from flaskskeleton.worker import BackgroundWorker
+
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 
 manager = Manager(app)
+
+
+@manager.command
+def start_background_worker():
+  """Start the background worker."""
+  worker = BackgroundWorker(interval=1)
+  log.info('Starting worker. Hit CTRL-C to exit!')
+  worker.start()
+  while worker.is_alive():
+    try:
+      worker.join(1)
+    except KeyboardInterrupt:
+      log.info('Shutting down worker thread!')
+      worker.stop()
 
 
 @manager.command

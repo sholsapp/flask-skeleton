3bb88c880e2b33ceca3dabb1bba7ebed731f7be0
Stephen Holsapple <sholsapp@gmail.com>
2016-07-08 13:58:15 -0700

    Add a logging wsgi layer.
    This is useful for debugging raw requests at the WSGI layer.

---
+++ flaskskeleton/middleware.py
@@ -0,0 +1,18 @@
+import pprint
+
+
+class LoggingMiddleware(object):
+    def __init__(self, app):
+        self._app = app
+
+    def __call__(self, environ, resp):
+        errorlog = environ['wsgi.errors']
+        pprint.pprint(('REQUEST', environ), stream=errorlog)
+        #pprint.pprint(environ['wsgi.input'].read(
+        #    int(environ['CONTENT_LENGTH'] or 0)))
+
+        def log_response(status, headers, *args):
+            pprint.pprint(('RESPONSE', status, headers), stream=errorlog)
+            return resp(status, headers, *args)
+
+        return self._app(environ, log_response)
